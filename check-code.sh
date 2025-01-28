0365431386bedd46986e72b9975ea4a209addcb8
Brian Clapper <bmc@clapper.org>
2025-01-28 15:29:24 -0500

    Added code checkers

---
+++ check-code.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+#
+# Run Python checkers and formatters.
+
+case $# in
+  0)
+    args="."
+    ;;
+  *)
+    args="$@"
+    ;;
+esac
+
+die() {
+  echo "$0: $1" >&2
+  exit 1
+}
+
+for i in $args
+do
+  echo "Checking types in $i"
+  if [ "$i" = "." ]
+  then
+    pyright findwords || die "pyright failed"
+  else
+    pyright $i || die "pyright failed"
+  fi
+
+  if [ "$i" = "." ]
+  then
+    echo "pycheck " findwords/*.py
+    pycheck findwords/*.py || die "pycheck failed"
+  else
+    echo "pycheck $i"
+    pycheck $i || die "pycheck failed"
+  fi
+
+  #echo "Sorting imports in $i"
+  #isort $i
+
+  #echo "Formatting $i with black"
+  #black $i
+done
